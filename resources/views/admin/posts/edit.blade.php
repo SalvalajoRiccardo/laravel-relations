f123bdee67cc208b49515b19008588a73b5ce2de
RiccardoSalvalajo <salvalajo.riccardo.cartiera@gmail.com>
2021-09-15 17:33:18 +0200

    errore 1

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -27,6 +27,17 @@
                     <div class="alert alert-danger">{{ $message }}</div>
                 @enderror            
             </div>
+            <div class="mb-3">
+                <label for="categ" class="form-label">Categories</label>
+                <select name="category_id" id="categ" class="form-control">
+                    <option value="">Seleziona una categoria</option>
+                    @foreach ($categories as $category)
+                        <option value="{{ $category->id }}" 
+                        @if ($category->id == old('$category_id', $post->$category_id))selected @endif>{{'$category->name'}}</option>
+                    @endforeach
+                </select>
+                          
+            </div>
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
     </div>
